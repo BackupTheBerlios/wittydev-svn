de4d209d1b4854aa5cf92aa9305e83cf1aea8662
helios <helios@f6e6994c-054b-0410-8f57-e087f6554c1c>
2008-05-23 16:55:09 +0000

    first

---
+++ trunk/TEST/src/org/wittydev/misctests/ServerInfoServiceTest.java
@@ -0,0 +1,34 @@
+package org.wittydev.misctests;
+
+
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
+
+import org.jnp.interfaces.NamingContext;
+import org.wittydev.j2ee.examples.templateA.bapp.ServerInfoService;
+
+public class ServerInfoServiceTest {
+
+	public static void main(String[] args) throws Exception{
+        
+		Hashtable environment = new Hashtable();
+		// The environment configuration depends on the application server the EJB is deployed on
+		// The following setting is related to an EJB 3.0 deployed on JBoss 4.2.1 Application server 
+		environment.put(Context.INITIAL_CONTEXT_FACTORY,"org.jnp.interfaces.NamingContextFactory");
+		environment.put(Context.URL_PKG_PREFIXES,"org.jboss.naming:org.jnp.interfaces");
+		environment.put(Context.PROVIDER_URL,"localhost:1210");
+
+		// Check the application server documentation for the EJB' JNDI name.  
+		String jndiName = "ServerInfoSessionBean/remote"; //ejb-name
+        
+		InitialContext context = new InitialContext(environment);
+		ServerInfoService service = (ServerInfoService)context.lookup(jndiName); 
+		
+		System.out.println("-->> "+service.getServerDate() );
+		System.out.println("-->> lookup object successfully");
+	}
+}
---
+++ trunk/TEST/src/org/wittydev/misctests/a.java
@@ -0,0 +1,5 @@
+package org.wittydev.misctests;
+
+public class a {
+
+}
